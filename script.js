f8e5bab56abf4528dbebe92bcf6411e3ffec9ce7
viplav59 <viplavkumar59@gmail.com>
2025-10-30 16:09:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll to top button
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
